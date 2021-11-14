5cf48a97d291376d24bf786c69746f2fc00ee90c
sittana afifi <sittana.afifi@ms.sd.zain.com>
2021-11-14 11:30:37 +0000

    edit view.py

--- catalog/views.py
+++ catalog/views.py
@@ -168,7 +168,7 @@ class BorrowBookListView(generic.ListView):
     template_name ='catalog/borrow_book_list.html'
     
     def get_queryset(self):
-        books=BookInstance.objects.all().filter(status='a').distinct('book')
+        books=BookInstance.objects.all().filter(status='a').distinct('book').values('pk')
         return books
 
 
